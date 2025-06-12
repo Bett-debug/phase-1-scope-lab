9ca14cabb988586fff479a7f0fa24bc5567ae394
Samuel <sam3bett@gmail.com>
2025-06-12 09:14:28 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  // Declare a variable in global scope
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  // Overwrite the global variable
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'alice';
+function changeLeastFavoriteCustomer() {
+  // Attempt to change the constant
+  leastFavoriteCustomer = 'charlie'; 
+}
